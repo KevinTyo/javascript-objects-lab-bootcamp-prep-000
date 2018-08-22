ca02bde3c46ba0e8779c36287029a96af7064c6f
kevin.d.tyo@gmail.com <kevin.d.tyo@gmail.com>
2018-08-22 17:25:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes =  {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  Object.assign({},object, {key: value})
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function destructivlyDeleteFromObjectByKey(object, key){
+  delete object.key;
+}
